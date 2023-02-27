650e87e5dca78a8e2c6e08b8ce101c348b84d37b
Axel <70969800+Zuxulius@users.noreply.github.com>
2023-02-27 21:27:54 +0100

    Pass all tests, 2 diff solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
+// const repeatString = function(string, n) {
+//   if (n<0) {
+//     return 'ERROR'
+//   } else return string.repeat(n).trimEnd()
+// };
+
 const repeatString = function(string, n) {
-  return string.repeat(n).trimEnd()
+  if (n<0) {
+    return 'ERROR'
+  } else {
+    let string_new = "";
+    for (i=0;i<n;i++) {
+      string_new = string_new + string;  
+  } console.log(string,n,string_new);
+    return string_new
+  }
 };
 
 // Do not edit below this line

362939eea69c539fb205a49c79d3b74a898c9b1d
Axel <70969800+Zuxulius@users.noreply.github.com>
2023-02-27 23:10:39 +0100

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+// Very verbose, can be done in one line
+const leapYears = function(year) {
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true
+  } else if (year % 400 === 0) return true
+  else return false
 };
 
 // Do not edit below this line

35b74c77bd898ac08ba50f699da22b4b501236a0
Axel <70969800+Zuxulius@users.noreply.github.com>
2023-02-27 21:46:10 +0100

    Pass third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+// const reverseString = function(word) {
+//   string = ""
+//   letters = word.split('');
+//   for (const letter of letters.reverse()) {
+//     string += letter;
+//   } console.log(string);
+//   return string
+// };
 
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
 };
 
 // Do not edit below this line

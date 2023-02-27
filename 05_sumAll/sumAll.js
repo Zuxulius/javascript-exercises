3d36bf482c1cb363979fe7863dcd06c0018f1a39
Axel <70969800+Zuxulius@users.noreply.github.com>
2023-02-27 22:39:46 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (typeof a != 'number' || typeof b != 'number' || a < 0 || b < 0) {
+  /* if (((typeof a || typeof b) != 'number') || ((a || b) < 0) { */
+    return 'ERROR'
+  }
+  result = 0;
+  high = Math.max(a, b)
+  low = Math.min(a, b)
 
+  while (low <= high) {
+    result += low;
+    low++;
+  } return result
 };
 
 // Do not edit below this line
